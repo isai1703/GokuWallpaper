f167117745c439d62aa1fb79111885f96638dd26
isai1703 <oscar_isai_170596@hotmail.com>
2025-11-15 07:59:17 -0600

    Add logging and error handling

--- app/src/main/java/com/oscar/gokuwallpaper/MainActivity.kt
+++ app/src/main/java/com/oscar/gokuwallpaper/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.oscar.gokuwallpaper
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        try {
+            Log.d("GokuWallpaper", "MainActivity onCreate started")
+            setContentView(R.layout.activity_main)
+            Toast.makeText(this, "Goku Wallpaper Loaded", Toast.LENGTH_SHORT).show()
+            Log.d("GokuWallpaper", "MainActivity onCreate finished")
+        } catch (e: Exception) {
+            Log.e("GokuWallpaper", "Error in onCreate", e)
+            Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+        }
     }
 }
