a44268866004b61f87b0325ba25316f33e289aaa
isai1703 <oscar_isai_170596@hotmail.com>
2025-11-15 07:52:53 -0600

    Simplify MainActivity to fix crash

--- app/src/main/java/com/oscar/gokuwallpaper/MainActivity.kt
+++ app/src/main/java/com/oscar/gokuwallpaper/MainActivity.kt
@@ -2,16 +2,11 @@ package com.oscar.gokuwallpaper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.oscar.gokuwallpaper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
